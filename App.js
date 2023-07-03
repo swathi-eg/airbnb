945b1ac6c49ebd232493b02ff1664d1e6d1b2380
Swathi R Devadiga <135304547+swathi-eg@users.noreply.github.com>
2023-07-03 17:18:01 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import SliderSelect from "./components/SliderSelect";
+import TenureSelect from "./components/TenureSelect";
+import Result from "./components/Result";
+import {Container, Grid } from "@mui/material";
+function App() {
+  const [data, setData] = useState({
+    homeValue: 3000,
+    downPayment: 3000 *0.2,
+    loanAmount: 3000 *0.8,
+    loanTerm: 5,
+    intrestRate:5
+  });
+
+ 
+  return (
+    <div className="App">
+      <Navbar />
+      <Container maxWidth="xl" sx={{ mt: 4 }}>
+        <Grid container spacing={5} alignItems='center'>
+          <Grid item xs={12} md={6}>
+            <SliderSelect data={data}setData={setData} />
+            <TenureSelect data={data}setData={setData}/>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Result data={data}/>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
